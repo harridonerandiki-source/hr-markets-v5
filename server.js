a852f876288c2be4c4315b3331bff6ce639c3edb
harridonerandiki-source <harridonerandiki@gmail.com>
2026-08-14 00:35:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -104,5 +104,7 @@ app.get("/api/admin/users", async (req, res) => {
     res.json(q.rows);
   } catch { res.status(500).json({ error: "Server error." }); }
 });
-
+app.get("/", (req, res) => {
+  res.send("HR Markets is running successfully 🚀");
+});
 app.listen(port, () => console.log(`HR Markets listening on ${port}`));
